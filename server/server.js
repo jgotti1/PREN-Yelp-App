9b5370914bed7e2e10c6a3cb6530673b91a8f8da
John Margotti <jgotti1@optonline.net>
2023-01-12 12:50:44 -0500

    initial backend API complete

--- server/server.js
+++ server/server.js
@@ -25,29 +25,67 @@ app.use(express.json());
 
 //Get all Restuarants
 app.get("/api/v1/restaurants", async (req, res) => {
-  const result = await db.query("SELECT * FROM restaurants");
-  res.send(result.rows);
-  console.log(`There are ${result.rowCount} records in our database`);
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    res.json(results.rows);
+    console.log(`There are ${results.rowCount} records in our database`);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 //Get a single Restuarant by ID
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  res.send(req.params);
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    // leaves us open to SQL injection below
+    // const result = await db.query(`SELECT * FROM restaurants WHERE id = ${req.params.id}`);
+    // correct
+    const result = await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 //UPDATE a single Restuarant by ID
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  res.send(req.body);
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const result = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *", [
+      req.body.name,
+      req.body.location,
+      req.body.price_range,
+      req.params.id,
+    ]);
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 //DELETE a single Restuarant by ID
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  res.send(req.params.id);
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    await db.query("DELETE FROM restaurants WHERE id = $1 RETURNING *", [req.params.id]);
+    res.json(result.rows)
+    
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
-//Create ne Restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  res.send(req.body);
+//Create new Restaurant
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const result = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *", [
+      req.body.name,
+      req.body.location,
+      req.body.price_range,
+    ]);
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // ********* END Routes ******************
