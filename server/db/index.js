646857a1430b508736d1a077378c1b989d6f5a3a
John Margotti <jgotti1@optonline.net>
2023-01-26 09:04:28 -0500

    deploy changes

--- server/db/index.js
+++ server/db/index.js
@@ -17,12 +17,4 @@ const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 
-  // production: {
-  //   user: process.env.RDS_USERNAME,
-  //   password: process.env.RDS_PASSWORD,
-  //   database: process.env.RDS_DB_NAME,
-  //   host: process.env.RDS_HOSTNAME,
-  //   port: process.env.RDS_PORT,
-  //   dialect: "postgres",
-  // },
 };
